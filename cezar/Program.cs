2f65c02552f8a54087b61ed4bad5bfa9007b1977
3109893 <3109893@TE-GDA-D-D36G15>
2022-04-07 12:14:53 +0200

    Dodaj pliki projektów.

---
+++ cezar/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("do zaszyfrowania");
+string a = Console.ReadLine();
+a = a.ToUpper();
+char[] achar = a.ToCharArray();
+int m;
+Console.WriteLine("klucz");
+m = Int32.Parse(Console.ReadLine());
+for (int e = 0; e < achar.Length; e++)
+{
+    achar[e] = (char)((achar[e] + m));
+    if (achar[e] > 90)
+    {
+        achar[e] = (char)((char)achar[e] - 26);
+    }
+
+}
+string b = new string(achar);
+Console.WriteLine("szyfrowana");
+Console.WriteLine(b);
